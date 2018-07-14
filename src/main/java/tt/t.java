48226663e91a7e986bae2157f262ec164ba6a19b
ysc1521 <yuanshancheng@meituan.com>
2018-07-14 18:14:16 +0800

    分支测试

--- src/main/java/tt/t.java
+++ src/main/java/tt/t.java
@@ -1,9 +1,10 @@
-package main.java;
+package main.java.tt;
 
-public class Demo {
+public class t {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("The End");
+        //test
     }
 
 }
